25006d58203d2122bb8b1ef9db7f6451e37e0a3d
Maciej Nalepa <mn.maciej.nalepa@outlook.com>
2025-04-16 20:03:53 +0200

    allowed cell selection

--- app/src/main/java/com/gummybearstudio/quicksudoku/ui/board/BoardFragment.kt
+++ app/src/main/java/com/gummybearstudio/quicksudoku/ui/board/BoardFragment.kt
@@ -11,6 +11,8 @@ import android.widget.Toast
 import androidx.gridlayout.widget.GridLayout
 
 import com.gummybearstudio.quicksudoku.R
+import com.gummybearstudio.quicksudoku.ui.board.BoardHelper.flatDecode
+import com.gummybearstudio.quicksudoku.ui.board.BoardHelper.flatEncode
 
 class BoardFragment : Fragment() {
     companion object {
@@ -25,9 +27,15 @@ class BoardFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel.state.observe(this, {
-            state -> Toast.makeText(requireContext(), state.toString(), Toast.LENGTH_SHORT).show()
-        })
+        viewModel.state.observe(this) { state ->
+            Toast.makeText(requireContext(), state.toString(), Toast.LENGTH_SHORT).show()
+        }
+        viewModel.selectedCell.observe(this) { cell ->
+            cellTextViews.forEach { textView ->
+                textView.setBackgroundColor(resources.getColor(R.color.purple_100))
+            }
+            cellTextViews[cell.flatEncode()].setBackgroundColor(resources.getColor(R.color.purple_200))
+        }
     }
 
     override fun onCreateView(
@@ -37,10 +45,11 @@ class BoardFragment : Fragment() {
         val inflatedView = inflater.inflate(R.layout.fragment_board, container, false)
 
         val mainGrid = inflatedView.findViewById<GridLayout>(R.id.mainGridLayout)
+        var nGrid = 0
         for (i in 0 until 3) {
             for (j in 0 until 3) {
                 val subGrid = GridLayout(requireContext())
-                buildGridLayout(subGrid)
+                buildGridLayout(subGrid, nGrid++)
                 mainGrid.addView(subGrid, createGridLayoutParams(i, j))
             }
         }
@@ -48,7 +57,7 @@ class BoardFragment : Fragment() {
         return inflatedView
     }
 
-    private fun buildGridLayout(grid: GridLayout) {
+    private fun buildGridLayout(grid: GridLayout, gridId: Int) {
         grid.columnCount = 3
         grid.rowCount = 3
         grid.setPadding(INNER_PADDING, INNER_PADDING, INNER_PADDING, INNER_PADDING)
@@ -60,6 +69,10 @@ class BoardFragment : Fragment() {
                 textView.textSize = 26f
                 textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
                 textView.setBackgroundColor(resources.getColor(R.color.purple_100))
+                textView.setOnClickListener {
+                    val startCell = (gridId * 9).flatDecode()
+                    viewModel.select(startCell.first + i, startCell.second + j)
+                }
                 grid.addView(textView, createGridLayoutParams(i, j))
                 cellTextViews.add(textView)
             }
