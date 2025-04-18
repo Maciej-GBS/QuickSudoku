94b2339e8af4a400c86b2115cf034867df2f387b
Maciej Nalepa <mn.maciej.nalepa@outlook.com>
2025-04-18 23:10:46 +0200

    connected viewmodel observer

--- app/src/main/java/com/gummybearstudio/quicksudoku/ui/board/BoardFragment.kt
+++ app/src/main/java/com/gummybearstudio/quicksudoku/ui/board/BoardFragment.kt
@@ -28,24 +28,43 @@ class BoardFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         viewModel.state.observe(this) { state ->
+            // TODO
             Toast.makeText(requireContext(), state.toString(), Toast.LENGTH_SHORT).show()
         }
-        viewModel.selectedCell.observe(this, ::callbackCellSelected)
-        viewModel.validFlags.observe(this, ::callbackCellValidity)
+        viewModel.selectedCell.observe(this) { cell -> callbackCellColorChanged(cell) }
+        viewModel.validFlags.observe(this) { flags -> callbackCellColorChanged(flags) }
         viewModel.cellValues.observe(this, ::callbackCellValues)
     }
 
-    private fun callbackCellSelected(cell: Pair<Int, Int>) {
-        cellTextViews.forEach { textView ->
-            textView.setBackgroundColor(resources.getColor(R.color.purple_100))
+    private fun callbackCellColorChanged(cellOrFlags: Any) {
+        var cell: Pair<Int, Int>? = null
+        var flags: List<Boolean>? = null
+        when (val v = cellOrFlags) {
+            is Pair<*, *> -> {
+                cell = cellOrFlags as Pair<Int, Int>
+                flags = viewModel.validFlags.value
+            }
+            is List<*> -> {
+                cell = viewModel.selectedCell.value
+                flags = cellOrFlags as List<Boolean>
+            }
         }
-        cellTextViews[cell.flatEncode()].setBackgroundColor(resources.getColor(R.color.purple_200))
-    }
-
-    private fun callbackCellValidity(validity: List<Boolean>) {
+        cellTextViews.forEach { textView ->
+            textView.setBackgroundColor(resources.getColor(R.color.white))
+        }
+        cell?.apply {
+            cellTextViews[this.flatEncode()]
+                .setBackgroundColor(resources.getColor(R.color.selection))
+        }
+        flags?.zip(cellTextViews)?.forEach {
+            if (!it.first) it.second.setBackgroundColor(resources.getColor(R.color.error))
+        }
     }
 
     private fun callbackCellValues(values: List<Int>) {
+        cellTextViews.zip(values).forEach {
+            it.first.text = it.second.toString()
+        }
     }
 
     override fun onCreateView(
@@ -71,14 +90,14 @@ class BoardFragment : Fragment() {
         grid.columnCount = 3
         grid.rowCount = 3
         grid.setPadding(INNER_PADDING, INNER_PADDING, INNER_PADDING, INNER_PADDING)
-        grid.setBackgroundColor(resources.getColor(R.color.purple_900))
+        grid.setBackgroundColor(resources.getColor(R.color.border))
         for (i in 0 until 3) {
             for (j in 0 until 3) {
                 val textView = CellTextView(requireContext())
                 textView.text = " "
                 textView.textSize = 26f
                 textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
-                textView.setBackgroundColor(resources.getColor(R.color.purple_100))
+                textView.setBackgroundColor(resources.getColor(R.color.white))
                 textView.setOnClickListener {
                     val startCell = (gridId * 9).flatDecode()
                     viewModel.select(startCell.first + i, startCell.second + j)
