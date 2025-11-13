59384c05542f81bace4cdc6803d16efc31ba7d72
Maciej Nalepa <mn.maciej.nalepa@outlook.com>
2025-11-13 19:52:01 +0100

    release build preparation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,21 +7,21 @@ plugins {
 
 android {
     namespace = "com.gummybearstudio.quicksudoku"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gummybearstudio.quicksudoku"
         minSdk = 30
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
